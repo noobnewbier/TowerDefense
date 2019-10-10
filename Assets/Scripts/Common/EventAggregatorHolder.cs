70f2d0e6a9143223d1e08782542398977baeb41f
noobnewbier7 <peter789b@gmail.com>
2019-10-10 01:38:27 +0100

    added a cheaky comment

--- Assets/Scripts/Common/EventAggregatorHolder.cs
+++ Assets/Scripts/Common/EventAggregatorHolder.cs
@@ -3,6 +3,7 @@ using EventManagement;
 
 namespace Common
 {
+    //Introduced only to not touch the code in sub module - what a genius
     public class EventAggregatorHolder
     {
         private static readonly Lazy<EventAggregator> LazyInstance = new Lazy<EventAggregator>(() => new EventAggregator());
