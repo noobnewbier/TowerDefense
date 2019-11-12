d0becf0c35ab3f3c926fe90139f029b9e5cbb681
noobnewbier7 <peter789b@gmail.com>
2019-11-12 02:00:37 +0000

    added unity-ml

--- Assets/Editor/TrainingBuildConfigSetter.cs
+++ Assets/Editor/TrainingBuildConfigSetter.cs
@@ -2,82 +2,79 @@
 using System.Linq;
 using Common;
 using UnityEditor;
-using UnityEngine;
 
-namespace Editor
+/// <summary>
+///     Adds the given define symbols to PlayerSettings define symbols.
+///     Just add your own define symbols to the Symbols property at the below.
+/// </summary>
+[InitializeOnLoad]
+// ReSharper disable once CheckNamespace
+public static class TrainingBuildConfigSetter
 {
-    /// <summary>
-    ///     Adds the given define symbols to PlayerSettings define symbols.
-    ///     Just add your own define symbols to the Symbols property at the below.
-    /// </summary>
-    [InitializeOnLoad]
-    public static class TrainingBuildConfigSetter
+    private const string MenuName = "Mode/Training";
+
+    private static bool IsEnabled
     {
-        private const string MenuName = "Mode/Training";
+        get => EditorPrefs.GetBool(MenuName, true);
+        set => EditorPrefs.SetBool(MenuName, value);
+    }
 
-        private static bool IsEnabled
+    private static readonly string[] TrainingSymbols =
+    {
+        GameConfig.TrainingMode
+    };
+
+    private static readonly string[] GameplaySymbols =
+    {
+        GameConfig.GameplayMode
+    };
+
+    [MenuItem(MenuName)]
+    private static void ToggleAction()
+    {
+        if (IsEnabled)
         {
-            get => EditorPrefs.GetBool(MenuName, true);
-            set => EditorPrefs.SetBool(MenuName, value);
+            OnToggleOff();
         }
-        
-        private static readonly string[] TrainingSymbols =
+        else
         {
-            GameConfig.TrainingMode
-        };
-        
-        private static readonly string[] GameplaySymbols =
-        {
-            GameConfig.GameplayMode
-        };
-        
-        [MenuItem(MenuName)]
-        private static void ToggleAction()
-        {
-            if (IsEnabled)
-            {
-                OnToggleOff();
-            }
-            else
-            {
-                OnToggleOn();
-            }
-
-            IsEnabled = !IsEnabled;
-            Menu.SetChecked(MenuName,IsEnabled);
+            OnToggleOn();
         }
 
-        [MenuItem(MenuName, true)]
-        private static bool ValidateToggleAction()
-        {
-            Menu.SetChecked(MenuName,IsEnabled);
-            return true;
-        }
+        IsEnabled = !IsEnabled;
+        Menu.SetChecked(MenuName, IsEnabled);
+    }
 
-        private static void OnToggleOn()
-        {
-            var allDefines = GetDefinedSymbols();
-            allDefines = allDefines.Union(TrainingSymbols).Except(GameplaySymbols);
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(
-                EditorUserBuildSettings.selectedBuildTargetGroup,
-                string.Join(";", allDefines.ToArray())
-            );
-        }
+    [MenuItem(MenuName, true)]
+    private static bool ValidateToggleAction()
+    {
+        Menu.SetChecked(MenuName, IsEnabled);
+        return true;
+    }
 
-        private static void OnToggleOff()
-        {
-            var allDefines = GetDefinedSymbols();
-            allDefines = allDefines.Except(TrainingSymbols).Union(GameplaySymbols);
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(
-                EditorUserBuildSettings.selectedBuildTargetGroup,
-                string.Join(";", allDefines.ToArray())
-            );
-        }
+    private static void OnToggleOn()
+    {
+        var allDefines = GetDefinedSymbols();
+        allDefines = allDefines.Union(TrainingSymbols).Except(GameplaySymbols);
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(
+            EditorUserBuildSettings.selectedBuildTargetGroup,
+            string.Join(";", allDefines.ToArray())
+        );
+    }
 
-        private static IEnumerable<string> GetDefinedSymbols()
-        {
-            var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            return definesString.Split(';').AsEnumerable();
-        }
+    private static void OnToggleOff()
+    {
+        var allDefines = GetDefinedSymbols();
+        allDefines = allDefines.Except(TrainingSymbols).Union(GameplaySymbols);
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(
+            EditorUserBuildSettings.selectedBuildTargetGroup,
+            string.Join(";", allDefines.ToArray())
+        );
+    }
+
+    private static IEnumerable<string> GetDefinedSymbols()
+    {
+        var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+        return definesString.Split(';').AsEnumerable();
     }
 }
