f64f07e7fcfd4674800f6645936594d53a323926
noobnewbier7 <peter789b@gmail.com>
2020-02-09 16:20:08 +0000

    directory restructuring

--- Assets/ml-agents/config/learn.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set %1
-set %2
-
-@echo on
-
-mlagents-learn --train --time-scale=100 --num-envs=3 --env=../../../Build/TowerDefense.exe --target-frame-rate=-1 --run-id=%RUNID% --quality-level=0 --curriculum=curricula/Suicide velocity_trainer_ppo_config.yaml %EXTRAARGS%
