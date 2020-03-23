efe67ec876257f62b7027e4691344ddd63247bd9
noobnewbier7 <peter789b@gmail.com>
2020-03-23 05:25:57 +0000

    fix a weird bug where when running in very high speed, "StartState"'s state enter is not called :()

--- Assets/Scripts/Statemachine/StartState.cs
+++ Assets/Scripts/Statemachine/StartState.cs
@@ -6,10 +6,10 @@ namespace Statemachine
 {
     public class StartState : StateMachineBehaviour
     {
-        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //for some reason, when running in a very high speed, onStateEnter is not called in THIS fucking state 
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            base.OnStateEnter(animator, stateInfo, layerIndex);
-
+            base.OnStateExit(animator, stateInfo, layerIndex);
             EventAggregatorHolder.Instance.Publish(new GameStartEvent());
         }
     }
